34fb72f3f1e93ec1fb9fc864afbb3d1c3b0df4fe
Danu Ardhiancahyo <danu.ardhiancahyo@gmail.com>
2021-08-02 21:06:14 +0700

    script katalon API testing

---
+++ Object Repository/Attendance - 02.Punch Out.rs
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Attendance - 02.Punch Out</name>
+   <tag></tag>
+   <elementGuidId>b53b306e-34df-4bce-899e-2c55d093bc36</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n  \&quot;id\&quot; : \&quot;015\&quot;,\n  \&quot;timezone\&quot; : \&quot;Jakarta\&quot;,\n  \&quot;note\&quot; : \&quot;Punch out\&quot;,\n  \&quot;datetime\&quot; : \&quot;2021-08-01_14:00\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer ${GlobalVariable.Token}</value>
+   </httpHeaderProperties>
+   <katalonVersion>7.9.1</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>http://qa.cilsy.id/symfony/web/index.php/api/v1/employee/015/punch-out</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/API Test.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8e9cf1c1-a93f-4520-a598-d7c851d7200f</testSuiteGuid>
+   <testCaseLink>
+      <guid>a66e1c4d-b579-4125-8ccc-167d9e53a47c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Small Project 3/Admin/Get Organization Info</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6aedb6df-7b85-4ea6-b0c0-d0be8c51344f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Small Project 3/Admin/Get User Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4ef27c1e-0273-4f4f-a2fa-8c783abb3326</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Small Project 3/Admin/User Login Invalid Credentials</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9f17bc0c-dda5-4118-b307-82018aa0bd3d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Small Project 3/Admin/User Login Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3a6dc55e-79e2-4e8d-988a-9902ecd7c659</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Small Project 3/Attandance/Punch In Invalid time zone</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e72f4760-0ffc-461e-972e-0a9690aeab02</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Small Project 3/Attandance/Punch Out Already punch out</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
